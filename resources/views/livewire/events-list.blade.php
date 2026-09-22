b798dc310d7c3ea80074db1af9934d9ce71159c8
Marc Schlagenhauf <marc@schlagenhauf.net>
2026-09-23 00:43:23 +0200

    Update events list

--- resources/views/livewire/events-list.blade.php
+++ resources/views/livewire/events-list.blade.php
@@ -35,35 +35,37 @@
                 <p class="form-text">{{ __('Click on an event to edit or delete it.') }}</p>
             </div>
             <div class="col-lg-9 order-lg-0">
-                <div class="list-group">
-                    @forelse ($this->events as $event)
-                    <a wire:key="event-{{ $event->id }}" href="{{ route('events.edit', $event->id) }}"
-                        class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="{{ $event->cancelled ? 'event-cancelled' : '' }}">
-                                <i class="event-icon fas
-                        fa-{{ $event->icon }}"
-                                    style="background-color:{{ config('ads.background-color', '#000000') }};
-                                   color:{{ $event->color }}"></i>&nbsp;<span
-                                    class="fw-bolder">{{ $event->name }}</span>
-                            </h4>
-                            <small>
-                                {!! $event->user
-                                ? $event->user->name
-                                : '<i class="fas fa-fw fa-robot" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="' .
-                                            __('Imported') .
-                                            '"></i>' !!}
-                            </small>
+                @forelse ($this->events as $event)
+                <div wire:key="event-{{ $event->id }}" class="card mb-2">
+                    <a href="{{ route('events.edit', $event->id) }}"
+                        class="card-header d-flex w-100 justify-content-between align-items-center text-body text-decoration-none">
+                        <span class="d-flex align-items-center gap-2 {{ $event->cancelled ? 'event-cancelled' : '' }}">
+                            <i class="event-icon fas fa-{{ $event->icon }}"
+                                style="background-color:{{ config('ads.background-color', '#000000') }};
+                                   color:{{ $event->color }}"></i>
+                            <span class="fw-bolder">{{ $event->name }}</span>
+                        </span>
+                        <small>
+                            {!! $event->user
+                            ? $event->user->name
+                            : '<i class="fas fa-fw fa-robot" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="' .
+                                        __('Imported') .
+                                        '"></i>' !!}
+                        </small>
+                    </a>
+                    <div class="card-body py-2">
+                        <div class="d-flex flex-wrap column-gap-3 row-gap-1 mb-2">
+                            <span class="event-start"><i
+                                    class="fas fa-fw fa-calendar"></i>&nbsp;{{ Carbon\Carbon::parse($event->real_start_date)->isoFormat('dddd LL') }}
+                                {{ $event->recurrence_description }}
+                            </span>
+                            <span class="event-start_time"><i
+                                    class="fas fa-fw fa-clock"></i>&nbsp;{{ substr($event->start_time, 0, 5) . ' - ' . substr($event->end_time, 0, 5) }}
+                            </span>
+                            <span class="event-place"><i
+                                    class="fas fa-fw fa-map-marked-alt"></i>&nbsp;{{ $event->place }}</span>
                         </div>
-                        <p class="event-start">
-                            {{ Carbon\Carbon::parse($event->real_start_date)->isoFormat('dddd LL') }}
-                            {{ $event->recurrence_description }}
-                        </p>
-                        <p class="event-start_time"><i
-                                class="fas fa-fw fa-clock"></i>&nbsp;{{ substr($event->start_time, 0, 5) . ' - ' . substr($event->end_time, 0, 5) }}
-                        </p>
-                        <p class="event-place"><i class="fas fa-fw fa-map-marked-alt"></i>&nbsp;{{ $event->place }}</p>
-                        <p>
+                        <div class="d-flex flex-wrap align-items-center column-gap-1 row-gap-1">
                             @if ($event->schedule?->disabled)
                             @include('badges.disabled')
                             @endif
@@ -89,24 +91,28 @@
                             @include('badges.imported_edited', ['event' => $event])
                             @endif
                             @endif
-                        </p>
-                        <button class="btn btn-sm btn-danger float-end"
-                            wire:click.prevent="deleteEvent({{ $event->id }})"
-                            wire:confirm="{{ __('Are you sure you want to delete the event ":EVENT_NAME"?', ['EVENT_NAME' => $event->name]) }}"
-                            data-bs-toggle="tooltip" data-bs-title="{{ __('Delete Event') }}">
-                            <i class="fas fa-fw fa-trash-can"></i>
-                        </button>
-                    </a>
-                    @empty
-                    @if ($search)
-                    <h3>{{ __('No events found') }}</h3>
-                    <button wire:click="clearSearch"
-                        class="btn btn-primary col-3">{{ __('Clear Search') }}</button>
-                    @else
-                    <h3>{{ __('No events found, how about creating one?') }}</h3>
-                    @endif
-                    @endforelse
+                            <a href="{{ route('events.edit', $event->id) }}" class="btn btn-sm btn-primary ms-auto"
+                                data-bs-toggle="tooltip" data-bs-title="{{ __('Edit Event') }}">
+                                <i class="fas fa-fw fa-pen-to-square"></i>
+                            </a>
+                            <button class="btn btn-sm btn-danger"
+                                wire:click="deleteEvent({{ $event->id }})"
+                                wire:confirm="{{ __('Are you sure you want to delete the event ":EVENT_NAME"?', ['EVENT_NAME' => $event->name]) }}"
+                                data-bs-toggle="tooltip" data-bs-title="{{ __('Delete Event') }}">
+                                <i class="fas fa-fw fa-trash-can"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
+                @empty
+                @if ($search)
+                <h3>{{ __('No events found') }}</h3>
+                <button wire:click="clearSearch"
+                    class="btn btn-primary col-3">{{ __('Clear Search') }}</button>
+                @else
+                <h3>{{ __('No events found, how about creating one?') }}</h3>
+                @endif
+                @endforelse
             </div>
         </div>
         <div class="col-12 justify-content-center">
